51eb1258a1960b1534e9d5578a715c0835951802
sanjay_pradhan <sanjay.p@nooor.in>
2020-09-08 19:29:43 +0530

    tick tac to finished logic

--- src/App.js
+++ src/App.js
@@ -22,7 +22,30 @@ const App = () => {
     itemArray.fill("empty", 0, 9)
   }
   const checkIsWinner = () => {
-
+    if (itemArray[0] === itemArray[1] && itemArray[0] === itemArray[2] && itemArray[0] !== "empty") {
+      setWinMessage(`${itemArray[0]} wins`)
+    }
+    else if (itemArray[3] !== "empty" && itemArray[3] === itemArray[4] && itemArray[4] === itemArray[5]) {
+      setWinMessage(`${itemArray[3]} wins`)
+    }
+    else if (itemArray[6] !== "empty" && itemArray[6] === itemArray[7] && itemArray[7] === itemArray[8]) {
+      setWinMessage(`${itemArray[6]} wins`)
+    }
+    else if (itemArray[0] !== "empty" && itemArray[0] === itemArray[3] && itemArray[3] === itemArray[6]) {
+      setWinMessage(`${itemArray[0]} wins`)
+    }
+    else if (itemArray[1] !== "empty" && itemArray[1] === itemArray[4] && itemArray[4] === itemArray[7]) {
+      setWinMessage(`${itemArray[1]} wins`)
+    }
+    else if (itemArray[2] !== "empty" && itemArray[2] === itemArray[5] && itemArray[5] === itemArray[8]) {
+      setWinMessage(`${itemArray[2]} wins`)
+    }
+    else if (itemArray[0] !== "empty" && itemArray[0] === itemArray[4] && itemArray[4] === itemArray[8]) {
+      setWinMessage(`${itemArray[0]} wins`)
+    }
+    else if (itemArray[2] !== "empty" && itemArray[2] === itemArray[4] && itemArray[4] === itemArray[6]) {
+      setWinMessage(`${itemArray[2]} wins`)
+    }
   }
   const changeItem = itemNumber => {
     if (winMessage) {
@@ -58,7 +81,7 @@ const App = () => {
               )}
             <div className="grid">
               {itemArray.map((item, index) => (
-                <Card key={index}>
+                <Card key={index} color="warning" onClick={() => changeItem(index)}>
                   <CardBody className="box">
                     <Icon name={item} />
                   </CardBody>
